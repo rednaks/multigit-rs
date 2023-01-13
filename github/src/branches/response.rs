823dd0c532650870f8798cd987fe107982190c79
SkanderBM <skander.bmahmoud@gmail.com>
2023-01-13 12:45:49 +0100

    fix

--- github/src/branches/response.rs
+++ github/src/branches/response.rs
@@ -1,11 +1,11 @@
-use crate::response::GithubCommit;
+use crate::commits::response::Commit;
 use serde_derive::{Deserialize, Serialize};
 use serde_json::Value;
 
 #[derive(Deserialize, Serialize, Debug)]
 pub struct Branch {
     pub name: String,
-    pub commit: GithubCommit,
+    pub commit: Commit,
     pub protected: bool,
     pub protection: Option<BranchProtection>,
     pub protection_url: Option<String>,
