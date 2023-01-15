225714610619dc41d96071aac93fbe6766c9a4a8
SkanderBM <skander.bmahmoud@gmail.com>
2023-01-15 13:41:55 +0100

    make config clonable

--- config/src/lib.rs
+++ config/src/lib.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Clone)]
 pub struct Config {
     pub token: String,
     pub org_name: String,
