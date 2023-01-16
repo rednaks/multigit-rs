416e4ed6def7347126671a698392a694e17b32bd
SkanderBM <skander.bmahmoud@gmail.com>
2023-01-16 22:33:40 +0100

    web-apis: get org repos

--- web/src/main.rs
+++ web/src/main.rs
@@ -6,7 +6,8 @@ use github::repos::response::Repo;
 use github::users::response::User;
 use github::Github;
 use log::error;
-use web_common::{OrgResponse, OrgType};
+use serde_derive::Deserialize;
+use web_common::{OrgResponse, OrgType, RepoResponse};
 
 struct AppState {
     gh: Github,
@@ -48,28 +49,39 @@ async fn root(data: web::Data<AppState>) -> impl Responder {
     HttpResponse::Ok().json(orgs_response)
 }
 
+#[derive(Deserialize)]
+pub struct OrgInfo {
+    #[serde(rename = "type")]
+    pub org_type: OrgType,
+}
+
 #[get("/api/orgs/{org}")]
-async fn manage_org(data: web::Data<AppState>, path: web::Path<String>) -> impl Responder {
+async fn manage_org(
+    data: web::Data<AppState>,
+    path: web::Path<String>,
+    org_info: web::Query<OrgInfo>,
+) -> impl Responder {
     let org_name: String = path.into_inner();
 
     println!("Managing: {}", org_name);
-    let the_org: Org = match data.gh.get_org(&org_name).await {
-        Ok(a_org) => a_org,
-        Err(e) => {
-            println!("Unable to get the org: {org_name}: {:?}", e.error_message());
-            ::std::process::exit(-1);
-        }
-    };
 
-    let repos: Vec<Repo> = match data.gh.list_repos(&the_org.login, &Some(false)).await {
+    let is_user = org_info.org_type == OrgType::User;
+
+    let repos: Vec<Repo> = match data.gh.list_repos(&org_name, &Some(is_user)).await {
         Ok(r) => r,
         Err(e) => {
             println!("Couldn't get repos: {}", e.error_message());
             ::std::process::exit(-1);
         }
     };
+    let repo_response: Vec<RepoResponse> = repos
+        .iter()
+        .map(|r| RepoResponse {
+            name: r.name.clone(),
+        })
+        .collect();
 
-    HttpResponse::Ok().json(repos)
+    HttpResponse::Ok().json(repo_response)
 }
 
 #[actix_web::main]
